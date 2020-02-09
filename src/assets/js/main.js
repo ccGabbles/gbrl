2848854393a991c096c451f35c7cca8bb79a6cdd
Moawad <gabriel.moawad@capgemini.com>
2020-02-09 15:52:46 +0100

    Random coloring with theming.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,9 @@
-// Placeholder
+const themes = ['orange', 'red', 'pink', 'blue', 'purple', 'green'];
+if (localStorage.previousTheme) {
+    themes.splice(themes.indexOf(localStorage.getItem("previousTheme")), 1);
+}
+
+const randomTheme = themes[Math.floor(Math.random() * themes.length)];
+localStorage.setItem("previousTheme", randomTheme);
+
+document.body.classList.add(randomTheme);
